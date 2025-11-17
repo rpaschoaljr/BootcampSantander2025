d4960983f4198ac08b97256727908091cc299014
Paschoal <r.paschoal@hotmail.com>
2025-11-17 19:08:47 -0300

    variaveis e constantes

---
+++ variaveis_constantes.py
@@ -0,0 +1,18 @@
+# Variáveis e Constantes em Python
+# Variáveis podem ser alteradas durante a execução do programa
+# Variáveis em python
+nome = "Jose"
+idade = 33
+print(f'Meu nome é {nome} e eu tenho {idade} anos.')
+
+nome = "Ricardo"
+print(f'Meu nome é {nome} e eu tenho {idade} anos.')
+
+limite_saque_diario = 1000
+
+# Constante em Python (convenção)
+BRAZILIAN_STATE = ['SP', 'RJ', 'MG', 'ES']
+print(BRAZILIAN_STATE)
+
+BRAZILIAN_STATE = 10  # Isso é possível, mas não recomendado
+print(BRAZILIAN_STATE)
