a2e5a36b7f235b955fed761951a0b9baa36ab010
Paschoal <r.paschoal@hotmail.com>
2025-11-18 10:28:49 -0300

    operadore de atribuição

---
+++ Tipos_de_operadores/operadores_atribuicao.py
@@ -0,0 +1,26 @@
+saldo = 500
+print(f'saldo = 500 R:{saldo}')
+
+saldo = 200
+print(f'saldo = 200 R:{saldo}')
+
+saldo += 10
+print(f'saldo += 10 R:{saldo}')
+
+saldo -= 5
+print(f'saldo -=5 R:{saldo}')
+
+saldo //= 2
+print(f'saldo //= 2 R:{saldo}')
+
+saldo /= 2
+print(f'saldo /= 2 R:{saldo}')
+
+saldo *= 10
+print(f'saldo *= 10 R:{saldo}')
+
+saldo %= 4
+print(f'saldo %= 4 R:{saldo}')
+
+saldo **= 2
+print(f'saldo **= 2 R:{saldo}')
