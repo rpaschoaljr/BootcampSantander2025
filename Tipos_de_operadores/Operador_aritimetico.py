5bd4a7d21868a640189f7ea4a90bce0ee5e47903
Paschoal <r.paschoal@hotmail.com>
2025-11-18 10:21:17 -0300

    Operadores aritimeticos

---
+++ Tipos_de_operadores/Operador_aritimetico.py
@@ -0,0 +1,40 @@
+#adição
+print(2 + 2)
+
+#subtração
+print(2 - 2)
+
+#multiplicação
+print(2 * 2)
+
+#divisão
+print(2 / 2)
+
+#divisão inteira
+print(2 // 2)
+
+#módulo
+print(10 % 3)
+
+#exponenciação
+print(2 ** 2)
+
+#ordem de precedência segue a matemática
+print(2 + 3 * 5)
+print((2 + 3) * 5)
+
+produto1 = 20
+produto2 = 10
+print(produto1 + produto2)
+print(produto1 - produto2)
+print(produto1 * produto2)
+print(produto1 / produto2)
+print(produto1 // produto2)
+print(produto1 % produto2)
+print(produto1 ** produto2)
+print((produto1 + produto2) * 5)
+
+x = (10 + 5) * 4
+y = (10 / 2) + 25 * ((2 - 2) ** 2)
+print(x)
+print(y)
