7ff117973c665276345b7c869415d69deecefaa8
Paschoal <r.paschoal@hotmail.com>
2025-11-18 10:39:32 -0300

    Operadores de associação

---
+++ Tipos_de_operadores/Operadores_de_associacao.py
@@ -0,0 +1,9 @@
+# Operadores de associação
+# in e not in
+# Verifica se um valor está presente em uma sequência (lista, tupla, string)
+frutas = ["limao", "uva"]
+curso = "Curso de python"
+
+print("laranja" not in frutas)
+print("limao" in frutas)
+print("Python" in curso)
