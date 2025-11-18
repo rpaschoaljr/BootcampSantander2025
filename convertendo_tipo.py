eec7bb183bb275fb284da57bc482185a58a328df
Paschoal <r.paschoal@hotmail.com>
2025-11-18 09:19:57 -0300

    conversão de tipos

---
+++ convertendo_tipo.py
@@ -0,0 +1,12 @@
+print(int(1.97784914))
+print(int("10"))
+print(float("1.97784914"))
+print(float(100))
+
+valor = 10
+valor_str = str(valor)
+print(type(valor))
+print(type(valor_str))
+
+print(100/2)
+print(100//2)
