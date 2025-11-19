e11fcc760f424b204b06185e763caf2503fdfefc
Paschoal <r.paschoal@hotmail.com>
2025-11-19 20:48:49 -0300

    Declarando listas

---
+++ Listas/00_declarando_listas.py
@@ -0,0 +1,16 @@
+# Declarando listas em Python
+
+frutas = ["laranja", "maca", "uva"]
+print(frutas)
+
+frutas = []
+print(frutas)
+
+letras = list("python")
+print(letras)
+
+numeros = list(range(10))
+print(numeros)
+
+carro = ["Ferrari", "F8", 4200000, 2020, 2900, "São Paulo", True]
+print(carro)
