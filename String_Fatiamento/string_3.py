632ecbf264a0e8622ddb4abe0ebd1c618d0b259a
Paschoal <r.paschoal@hotmail.com>
2025-11-19 20:32:49 -0300

    string parte 3 fatiamento

---
+++ String_Fatiamento/string_3.py
@@ -0,0 +1,12 @@
+# String Fatiamento - Parte 3
+
+nome = "Ricardo Paschoal Junior"
+
+print(nome[0])#primeira letra
+print(nome[-2])#penultima letra
+print(nome[:9])#primeiras 9 letras
+print(nome[10:])#da 10 letra até o final
+print(nome[10:16])#da 10 letra até a 16
+print(nome[10:16:2])#da 10 letra até a 16, pulando de 2 em 2
+print(nome[:])#toda a string
+print(nome[::-1])#string invertida
