04128a9341046e5959ce29d891918abf94886afe
Paschoal <r.paschoal@hotmail.com>
2025-11-20 17:33:32 -0300

    feat: add Python script demonstrating list clearing methods

---
+++ Listas/08_clear.py
@@ -0,0 +1,15 @@
+# Clear
+# Limpar lista
+lista = [1, "Python", [40, 30, 20]]
+
+print(lista)  # [1, "Python", [40, 30, 20]]
+
+lista.clear()
+
+print(lista)  # []
+
+lista_2 = [1, 2, 3]
+print(lista_2)
+
+lista_2 = []  # Podemos redeclarar ela vazia também, muito cuidado com isso
+print(lista_2)
